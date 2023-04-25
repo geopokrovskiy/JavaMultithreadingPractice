12123c8bcff82e9937c31a46dad542a46a2c930c
geopokrovskiy <99304283+geopokrovskiy@users.noreply.github.com>
2023-04-26 01:11:46 +0200

    Run methods are now synchronized

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,21 +10,27 @@ public class Main {
         Runnable thread1 = new Runnable() {
             @Override
             public void run() {
-                foo.first(this);
+                synchronized (foo) {
+                    foo.first(this);
+                }
             }
         };
 
         Runnable thread2 = new Runnable() {
             @Override
             public void run() {
-                foo.second(this);
+                synchronized (foo) {
+                    foo.second(this);
+                }
             }
         };
 
         Runnable thread3 = new Runnable() {
             @Override
             public void run() {
-                foo.third(this);
+                synchronized (foo) {
+                    foo.third(this);
+                }
             }
         };
 
@@ -37,33 +43,33 @@ public class Main {
         System.out.println();
 
         try(ScheduledExecutorService service2 = Executors.newScheduledThreadPool(3)){
-            service2.schedule(thread1, 10, TimeUnit.MILLISECONDS);
-            service2.schedule(thread3, 30, TimeUnit.MILLISECONDS);
-            service2.schedule(thread2, 20, TimeUnit.MILLISECONDS);
+            service2.schedule(thread1, 100, TimeUnit.MILLISECONDS);
+            service2.schedule(thread3, 300, TimeUnit.MILLISECONDS);
+            service2.schedule(thread2, 200, TimeUnit.MILLISECONDS);
         }
 
         System.out.println();
 
         try(ScheduledExecutorService service3 = Executors.newScheduledThreadPool(3)){
-            service3.schedule(thread2, 20, TimeUnit.MILLISECONDS);
-            service3.schedule(thread1, 10, TimeUnit.MILLISECONDS);
-            service3.schedule(thread3, 30, TimeUnit.MILLISECONDS);
+            service3.schedule(thread2, 200, TimeUnit.MILLISECONDS);
+            service3.schedule(thread1, 100, TimeUnit.MILLISECONDS);
+            service3.schedule(thread3, 300, TimeUnit.MILLISECONDS);
         }
 
         System.out.println();
 
         try(ScheduledExecutorService service4 = Executors.newScheduledThreadPool(3)){
-            service4.schedule(thread2, 20, TimeUnit.MILLISECONDS);
-            service4.schedule(thread3, 30, TimeUnit.MILLISECONDS);
-            service4.schedule(thread1, 10, TimeUnit.MILLISECONDS);
+            service4.schedule(thread2, 200, TimeUnit.MILLISECONDS);
+            service4.schedule(thread3, 300, TimeUnit.MILLISECONDS);
+            service4.schedule(thread1, 100, TimeUnit.MILLISECONDS);
         }
 
         System.out.println();
 
         try(ScheduledExecutorService service5 = Executors.newScheduledThreadPool(3)){
-            service5.schedule(thread3, 30, TimeUnit.MILLISECONDS);
-            service5.schedule(thread1, 10, TimeUnit.MILLISECONDS);
-            service5.schedule(thread2, 20, TimeUnit.MILLISECONDS);
+            service5.schedule(thread3, 300, TimeUnit.MILLISECONDS);
+            service5.schedule(thread1, 100, TimeUnit.MILLISECONDS);
+            service5.schedule(thread2, 200, TimeUnit.MILLISECONDS);
         }
 
         System.out.println();
