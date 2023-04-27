d11181788122b1179804f781543db625f535d317
geopokrovskiy <99304283+geopokrovskiy@users.noreply.github.com>
2023-04-27 21:27:22 +0200

    The program has been rewritten with the use of semaphores

--- src/main/java/org/example/Foo.java
+++ src/main/java/org/example/Foo.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import java.util.concurrent.Semaphore;
+
 public class Foo {
-    public void first(Runnable r){
+    private Semaphore sem1 = new Semaphore(1);
+    private Semaphore sem2 = new Semaphore(0);
+    private Semaphore sem3 = new Semaphore(0);
+    public void first(Runnable r) throws InterruptedException {
+        sem1.acquire();
         System.out.print("first");
+        sem2.release();
     }
-    public void second(Runnable r){
+    public void second(Runnable r) throws InterruptedException {
+        sem2.acquire();
         System.out.print("second");
+        sem3.release();
     }
-    public void third(Runnable r){
+    public void third(Runnable r) throws InterruptedException {
+        sem3.acquire();
         System.out.print("third");
+        sem1.release();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,83 +1,53 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
-        Runnable thread1 = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (foo) {
-                    foo.first(this);
-                }
-            }
-        };
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Thread t1 = new Thread("Thread1");
+        Thread t2 = new Thread("Thread2");
+        Thread t3 = new Thread("Thread3");
 
-        Runnable thread2 = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (foo) {
-                    foo.second(this);
-                }
-            }
-        };
 
-        Runnable thread3 = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (foo) {
-                    foo.third(this);
-                }
-            }
-        };
+        try {
+            foo.first(t1);
+            foo.second(t2);
+            foo.third(t3);
 
-        try(ScheduledExecutorService service1 = Executors.newScheduledThreadPool(3)){
-            service1.schedule(thread1, 100, TimeUnit.MILLISECONDS);
-            service1.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-            service1.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-        }
+            System.out.println();
 
-        System.out.println();
+            foo.first(t1);
+            foo.second(t3);
+            foo.third(t2);
 
-        try(ScheduledExecutorService service2 = Executors.newScheduledThreadPool(3)){
-            service2.schedule(thread1, 100, TimeUnit.MILLISECONDS);
-            service2.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-            service2.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-        }
+            System.out.println();
 
-        System.out.println();
+            foo.first(t2);
+            foo.second(t1);
+            foo.third(t3);
 
-        try(ScheduledExecutorService service3 = Executors.newScheduledThreadPool(3)){
-            service3.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-            service3.schedule(thread1, 100, TimeUnit.MILLISECONDS);
-            service3.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-        }
+            System.out.println();
 
-        System.out.println();
+            foo.first(t2);
+            foo.second(t3);
+            foo.third(t1);
 
-        try(ScheduledExecutorService service4 = Executors.newScheduledThreadPool(3)){
-            service4.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-            service4.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-            service4.schedule(thread1, 100, TimeUnit.MILLISECONDS);
-        }
+            System.out.println();
 
-        System.out.println();
+            foo.first(t3);
+            foo.second(t1);
+            foo.third(t2);
 
-        try(ScheduledExecutorService service5 = Executors.newScheduledThreadPool(3)){
-            service5.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-            service5.schedule(thread1, 100, TimeUnit.MILLISECONDS);
-            service5.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-        }
+            System.out.println();
 
-        System.out.println();
-
-        try(ScheduledExecutorService service6 = Executors.newScheduledThreadPool(3)){
-            service6.schedule(thread3, 300, TimeUnit.MILLISECONDS);
-            service6.schedule(thread2, 200, TimeUnit.MILLISECONDS);
-            service6.schedule(thread1, 100, TimeUnit.MILLISECONDS);
+            foo.first(t3);
+            foo.second(t2);
+            foo.third(t1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
